df9b0a524d4be3f18d3f8f2f5925d571d2de4e67
brianfdevore <brianfdevore@gmail.com>
2020-08-10 16:35:41 -0400

    v4 2

--- website/scripts/clean_dir.sh
+++ website/scripts/clean_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-isExistFile = 'find /var/www/html/index.php'
+isExistFile = '/var/www/html/index.php'
 
-if [[ -n $isExistFile ]]; then
+if [[ -e $isExistFile ]]; then
     rm /var/www/html/index.php
 fi
